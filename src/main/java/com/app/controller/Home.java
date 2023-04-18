313a49fc15ec266968e63f100d9cc2041ba18c6f
manesunil <manesunil069@gmail.com>
2023-04-18 19:11:42 +0530

    COMMIT -1

---
+++ src/main/java/com/app/controller/Home.java
@@ -0,0 +1,17 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class Home {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "home";
+    }
+
+}
